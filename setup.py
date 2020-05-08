80d4f854d5714094bd628d89c3a39605b4b89928
pankaj <pankajthekush@gmail.com>
2020-05-08 06:10:21 -0400

    version bump

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ import sys
 
 setup(
     name='pydocker',
-    version='1.0',
+    version='1.1',
     packages=['pydocker'],
-    entry_points ={'console_scripts': ['dcreate = pydocker.pydocker:docrun']}
+    entry_points ={'console_scripts': ['drun = pydocker.pydocker:docrun',
+                                        'dcreate=pydocker.pydocker:docreate']}
 )
