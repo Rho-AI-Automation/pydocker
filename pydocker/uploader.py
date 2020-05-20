3f7429396837c064355afc14f415e359dac350d4
pankaj <pankajthekush@gmail.com>
2020-05-20 14:02:51 +0530

    update

--- pydocker/uploader.py
+++ pydocker/uploader.py
@@ -11,8 +11,20 @@ from sqlalchemy.orm import sessionmaker
 
 #custome import
 from shelper import pgconnstring
+from supload.supload import upload_file
+from pydocker import config_file
 
 #global variables
+jobj = config_file('config.json')
+s3_bucket = jobj['s3_bucket_name']
+s3_folder = jobj['s3_folder']
+
+total_passed = 0
+total_failed = 0
+
+failed_files = list()
+success_files = list()
+
 localsession = None
 local_engine = None
 
@@ -65,25 +77,42 @@ def update_remote(link):
     data = remotesession.query(RemoteTable).filter(RemoteTable.t_link ==link) #pylint: disable=maybe-no-member
     
     for row in data:
-        row.t_status ='UPLOADED2'
+        row.t_status ='UPLOADED'
         remotesession.commit() #pylint: disable=maybe-no-member
 
 
-def upload_remote(link):
+def upload_remote(link,local_file):
     """
     tries to insert the link to local db, if already present 
     then do nothing but if insert is successufl , update the status
     on remote db
 
     """
+    global total_passed
+    global total_failed
+    global failed_files
+    global success_files
+
     query_res_count = len(list(localsession.query(upload_table.link).filter(upload_table.link==link))) #pylint: disable=maybe-no-member
     
     if query_res_count == 0:
-        new_ua = upload_table(link=link,updatedon=datetime.datetime.now())
-        localsession.add(new_ua) #pylint: disable=maybe-no-member
-        localsession.commit() #pylint: disable=maybe-no-member
-        #update the remote db
-        update_remote(link=link)
+        correct_upload =  upload_file(local_file,s3_folder,s3_bucket)
+     
+  
+        if correct_upload:
+            total_passed += 1
+            #if s3 upload is successful then only add the data to local table 
+            #and update remote table
+            new_ua = upload_table(link=link,updatedon=datetime.datetime.now())
+            localsession.add(new_ua) #pylint: disable=maybe-no-member
+            localsession.commit() #pylint: disable=maybe-no-member
+            #update the remote db
+            update_remote(link=link)
+            success_files.append(local_file)
+        else:
+            total_failed +=1
+            failed_files.append(local_file)
+
     else:
         return False
 
@@ -114,7 +143,7 @@ def watch_folder():
         all_html_files =    glob.glob(os.path.join(directory,'*.html'))
         for html_file in all_html_files:
             link_name = get_link_from_html(link=html_file)
-            upload_remote(link_name)
+            upload_remote(link_name,html_file)
 
 
         dictdata[directory] = len(all_html_files)
@@ -124,10 +153,26 @@ def watch_folder():
 
     for k,v in dictdata.items():
         print ("{:<30} {:<15}".format(k.replace('/',''), v),flush=True)
-    print(f'updated on :{datetime.datetime.now()}')
-    localsession.commit()
-    remotesession.commit()
+
+    print('******************************************************')
+    localsession.commit() #pylint: disable=maybe-no-member
+    remotesession.commit() #pylint: disable=maybe-no-member
     close_all_sessions() #close session
+    
+    set_actual_fail_upload = set(failed_files).difference(set(success_files)) 
+    print('failed upload')
+    print('----------')
+    for sq,el in enumerate(set_actual_fail_upload):
+        print(f'{sq}.{el}')
+    print('******************************************')
+    print('success upload')
+    print('----------')
+    for sq,el in enumerate(success_files):
+        print(f'{sq}.{el}')
+    print(f'total success upload : {total_passed} total failed upload: {total_failed}')
+    print(f'updated on :{datetime.datetime.now()}')
+    success_files.clear()
+
     
 
 
