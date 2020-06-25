1337ace5477cc4fbd7d1a5e6aaeda639a5f2d012
pankaj <pankajthekush@gmail.com>
2020-06-25 17:28:36 +0530

    upload

--- pydocker/uploader.py
+++ pydocker/uploader.py
@@ -189,6 +189,7 @@ def watch_folder(force_upload=True):
     for directory in sub_directs:
         all_html_files =  glob.glob(os.path.join(directory,'*.html'))
         for html_file in all_html_files:
+            print(f'uploading {html_file}')
             link_name = get_link_from_html(link=html_file)
 
             if link_name == 'ERROR':
