5572c9f2eb1d8278bf7dce9af415eb0b24222e96
root <pankajthekush@gmail.com>
2020-10-27 01:25:55 +0530

    updated node

--- pydocker/pydocker.py
+++ pydocker/pydocker.py
@@ -486,6 +486,7 @@ def uchecker_run_crawlera(container_name,image_name):
     print('---------')
     print(stdout.decode('utf-8'))
 
+    run_command(buckt_name=container_name,command_name='node /prerender/server.js',screen_name='prerender')
     run_command(buckt_name=container_name,command_name='grunner',screen_name='grunner')
    
  
